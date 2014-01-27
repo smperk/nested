f6e12c9148829340a70a9699271fdfec6e70db68
Sean Maurice Perkins <sean.perkins@rcn.net>
2014-01-27 15:59:55 -0500

    Q&A

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,4 +1,5 @@
 class Question < ActiveRecord::Base
-  has_many :questions, :dependent => :destroy
-  accepts_nested_attributes_for :questions
+ belongs_to :survey
+ has_many :answers, :dependent => :destroy
+ accepts_nested_attributes_for :answers, :reject_if => lambda { |a| a[:content].blank? }, :allow_destroy => true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140127180251) do
+ActiveRecord::Schema.define(:version => 20140127200855) do
+
+  create_table "answers", :force => true do |t|
+    t.integer  "question_id"
+    t.string   "content"
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
+  end
 
   create_table "questions", :force => true do |t|
     t.integer  "survey_id"
