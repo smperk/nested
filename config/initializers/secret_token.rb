52ffc581159210b3b43c20359b0dd9edad6112c4
Sean Maurice Perkins <sean.perkins@rcn.net>
2014-01-27 13:57:05 -0500

    first

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Surveysays::Application.config.secret_token = '84d922636f66253ebd7adb152700b68c8af6699f17372505ce196a30b82e8ae3113a9db54853395f30fb3119c22040cc751287f13acd3a8798fadce0d01c4d9d'
